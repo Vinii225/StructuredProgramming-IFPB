630224939891d8373940a0de128cdce350e2894c
Vinícius Ares <vinicius.ares12@gmail.com>
2024-11-28 19:54:56 -0300

    Delete Sequential-Lab/Question1.py

--- Sequential-Lab/Question1.py
+++
@@ -1,8 +0,0 @@
-# Write a whole positive number and calculate if it's pair or not.
-number1=int(input("Type your number: "))
-
-# Test if number is pair or not...
-if number1 % 2 ==0:
-    print(f"{number1} is pair.")
-else:
-    print(f"{number1} is odd.")
