9123ce828c3d383adbb5abd45a46d5d93e77939d
Vinícius Ares <vinicius.ares12@gmail.com>
2024-11-28 21:03:19 -0300

    Question-1

--- Sequential-Lab/Q1.py
+++ Sequential-Lab/Q1.py
@@ -1,7 +1,9 @@
-# Write a whole positive number and calculate if it's pair or not.
+# Read a whole positive number and calculate if it's pair or not.
+
+# Get number
 number1=int(input("Type your number: "))
 
-# Test if number is pair or not...
+# Test if number is pair or not and show it.
 if number1 % 2 ==0:
     print(f"{number1} is pair.")
 else:
---
+++ Sequential-Lab/Q5.py
@@ -0,0 +1,24 @@
+# One enterprise defined different cashbacks based on purchase price, show cashback in the final.
+
+#                       Purchase Value - Cashback
+#                            Until 100.00 -> 4%
+#               Between 100.01 and 200.00 -> 6%
+#               Between 200.01 and 400.00 -> 8%
+#                            Above 400.00 -> 10%
+
+# Get purchase value.
+purchase_value=float(input("Type your purchase value: "))
+
+# Verify cashback.
+if purchase_value<=100:
+    cashback_value = purchase_value * 0.04
+elif purchase_value<=200:
+    cashback_value = purchase_value * 0.06
+elif purchase_value<=400:
+    cashback_value = purchase_value * 0.08
+elif purchase_value>400:
+    cashback_value = purchase_value * 0.10
+
+# Show cashback.
+print(f"The cashback is: {cashback_value:.2f}")
+
